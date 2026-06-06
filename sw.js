e7c7f8fd35ff7773f6b9b2687621d08b2d290ad0
DooYeon <dooey8@gmail.com>
2026-06-06 16:00:45 +0000

    invoice PWA + dashboard PWA + 민감폴더 차단

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='ib-invoice-v1';
+const ASSETS=['./invoice.html','./icon-180.png','./icon-512.png','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(C).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./invoice.html'))));});
