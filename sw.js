072306ae5e6ffff3c8e9812f62b1737293c81cdd
DooYeon <dooey8@gmail.com>
2026-06-07 03:33:27 -0400

    web update 2026-06-07 03:33

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='ib-invoice-v2';
-const ASSETS=['./invoice.html','./icon-180.png','./icon-512.png','./manifest.webmanifest'];
+const C='ib-invoice-v3';
+const ASSETS=['./invoice.html','./assets/icon-180.png','./assets/icon-512.png','./assets/manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(C).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./invoice.html'))));});
